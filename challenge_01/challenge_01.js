bcd750376aa8c8a05b91cee530e91d80379d9650
Paul Hendea <paulhendea@outlook.com>
2023-11-11 00:49:00 +0100

    rename folder

--- challenge_01/challenge_01.js
+++ challenge_01/challenge_01.js
@@ -4,12 +4,12 @@ import { fileURLToPath } from 'url'
 // Get the message
 // by using fileUrlToPath works in windows too
 const messagePath = fileURLToPath(import.meta.resolve('./message_01.txt'))
-const CODE = fs.readFileSync(messagePath, 'utf-8')
+const code = fs.readFileSync(messagePath, 'utf-8')
 
 // Decode
 // a Map in JS ensures the entries are in the insertion order
 const WORDS = new Map()
-CODE.split(' ').forEach((word) => {
+code.split(' ').forEach((word) => {
   const key = word.toLowerCase()
   if (WORDS.has(key)) WORDS.set(key, WORDS.get(key) + 1)
   else WORDS.set(key, 1)
